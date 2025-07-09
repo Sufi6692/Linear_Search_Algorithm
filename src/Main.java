846c361eaa254249c817e97029bfcd470f46775d
Sufi6692 <sayedsufikhalandar@gmail.com>
2025-07-09 22:12:55 +0530

    Searched for an target number in array

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,31 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        // Linear_Search_Algorithm we are going to learn and also solve some problems with practice
+
+        int [] nums = {23, 45, 1, 2, 8, 19, -3, 16, -11, 28};
+        int target = 11;
+        int ans = linerSearch(nums,target);
+        System.out.println(ans);
+    }
+
+    //search in the array : return the index if item found
+    // otherwise if item not found return -1
+
+    static int linerSearch(int[] arr, int target){
+        if(arr.length== 0){
+            return -1;
         }
+        //run a for loop
+        for (int index = 0; index <arr.length;index++){
+            //check for element at every index if it is = target
+            int element = arr[index];
+            if (element == target){
+                return index;
+            }
+        }
+        // this line will execute if none of the return statement above have executed
+        //hence the target  not found
+        return -1;
     }
 }
