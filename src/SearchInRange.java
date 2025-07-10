97b6a2bdd95fbe31ab37223a9c6569899ff564b8
Sufi6692 <sayedsufikhalandar@gmail.com>
2025-07-10 07:12:58 +0530

    Searched for a target in Range

---
+++ src/SearchInRange.java
@@ -0,0 +1,24 @@
+public class SearchInRange {
+    public static void main(String[] args) {
+        int[] arr ={18,12,-7,3,14,28};
+        int target = 18;
+        System.out.println(linerSearch(arr,target,1,4));
+    }
+    static int linerSearch(int[]arr, int target, int start, int end){
+        if(arr.length == 0){
+            return -1;
+        }
+
+        // run a for loop
+        for (int index = start; index <= end; index++){
+            //check for element at every index if it is = target
+            int element = arr[index];
+            if(element == target){
+                return index;
+            }
+        }
+        //this line will execute if none of the return  statement above have executed
+        //hence the target not
+        return  -1;
+    }
+}
