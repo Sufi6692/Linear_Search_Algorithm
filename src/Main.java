7c6132a696cb03b476aaea873a47bcf30f3ba9d0
Sufi6692 <sayedsufikhalandar@gmail.com>
2025-07-10 06:47:32 +0530

    Searched for an target number in array

--- src/Main.java
+++ src/Main.java
@@ -17,15 +17,20 @@ public class Main {
             return -1;
         }
         //run a for loop
-        for (int index = 0; index <arr.length;index++){
-            //check for element at every index if it is = target
-            int element = arr[index];
-            if (element == target){
-                return index;
+//        for (int index = 0; index <arr.length;index++){
+//            //check for element at every index if it is = target
+//            int element = arr[index];
+//            if (element == target){
+//                return index;
+//            }
+//        }
+        for(int element:arr){
+            if (element == target) {
+                return  element;
             }
         }
         // this line will execute if none of the return statement above have executed
         //hence the target  not found
-        return -1;
+        return Integer.MAX_VALUE;
     }
 }
